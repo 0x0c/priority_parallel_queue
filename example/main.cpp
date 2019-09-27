ca0c2b4af18636a9854123e7bc2b44383ddfa49e
Akira <akira.matsuda@me.com>
2019-09-27 15:11:05 +0900

    Bug fix.

--- example/main.cpp
+++ example/main.cpp
@@ -6,43 +6,78 @@ using namespace std;
 class message
 {
 public:
-    message(std::string text) {
+    explicit message(std::string text) {
         this->text = text;
     }
+    
+    message(const message &m) = default;
+    message(message&& m) = default;
+    message& operator=(message&&) = default;
+    message& operator=(message const&) = default;
     
     std::string text;
 };
 
 int main()
 {
-    auto q = m2d::priority_parallel_queue<std::string>();
-    q.push("11", 1);
-    q.push("12", 1);
+    auto q = m2d::priority_parallel_queue<message>();
+    q.push(message("11"), 1);
+    q.push(message("12"), 1);
     
-    q.push("21", 2);
-    q.push("22", 2);
+    q.push(message("21"), 2);
+    q.push(message("22"), 2);
     
-    q.push("13", 1);
-    q.push("14", 1);
+    q.push(message("13"), 1);
+    q.push(message("14"), 1);
     
-    q.push("23", 2);
-    q.push("24", 2);
+    q.push(message("23"), 2);
+    q.push(message("24"), 2);
     
-    q.push("31", 3);
-    q.push("32", 3);
+    q.push(message("31"), 3);
+    q.push(message("32"), 3);
     
-    q.push("33", 3);
-    q.push("25", 2);
-    q.push("15", 1);
+    q.push(message("33"), 3);
+    q.push(message("25"), 2);
+    q.push(message("15"), 1);
     
-    q.push("34", 3);
-    q.push("35", 3);
+    q.push(message("34"), 3);
+    q.push(message("35"), 3);
     
     while(!q.empty()) {
         auto m = q.top();
-        std::cout << m << std::endl;
+        std::cout << m.text << std::endl;
         q.pop();
     }
+
+    
+//    auto q = m2d::priority_parallel_queue<std::string>();
+//    q.push("11", 1);
+//    q.push("12", 1);
+//
+//    q.push("21", 2);
+//    q.push("22", 2);
+//
+//    q.push("13", 1);
+//    q.push("14", 1);
+//
+//    q.push("23", 2);
+//    q.push("24", 2);
+//
+//    q.push("31", 3);
+//    q.push("32", 3);
+//
+//    q.push("33", 3);
+//    q.push("25", 2);
+//    q.push("15", 1);
+//
+//    q.push("34", 3);
+//    q.push("35", 3);
+//
+//    while(!q.empty()) {
+//        auto m = q.top();
+//        std::cout << m << std::endl;
+//        q.pop();
+//    }
 
     return 0;
 }
--- include/priority_parallel_queue.h
+++ include/priority_parallel_queue.h
@@ -6,22 +6,28 @@
 namespace m2d
 {
 template <typename T>
-bool operator< (const std::tuple<int, int, T> &t1, const std::tuple<int, int, T> &t2){
-    int priority1 = std::get<0>(t1);
-    int counter1 = std::get<1>(t1);
-    int priority2 = std::get<0>(t2);
-    int counter2 = std::get<1>(t1);
-    
-    return (priority1 < priority2) && (counter1 < counter2);
+struct tuple_less {
+    bool operator() (const std::tuple<int, int, T> &t1, const std::tuple<int, int, T> &t2){
+        int priority1 = std::get<0>(t1);
+        int counter1 = std::get<1>(t1);
+        
+        int priority2 = std::get<0>(t2);
+        int counter2 = std::get<1>(t2);
+        
+        if (priority1 == priority2) {
+            return counter1 > counter2;
+        }
+        return priority1 < priority2;
+    }
 };
 
 template <typename T>
 class priority_parallel_queue
 {
 private:
-    using Q = std::tuple<int, int, T>;
+    using Q = std::tuple<int, int, T>; // prioriy, counter, value
     
-    std::priority_queue<Q> queue;
+    std::priority_queue<Q, std::vector<Q>, tuple_less<T> > queue;
     int counter = 0;
     
 public:
