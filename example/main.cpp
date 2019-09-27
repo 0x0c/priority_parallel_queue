945541239edd325a4f4ac87cc0eafd524cc49f48
Akira <akira.matsuda@me.com>
2019-09-27 15:12:31 +0900

    コメントを消した

--- example/main.cpp
+++ example/main.cpp
@@ -49,35 +49,5 @@ int main()
         q.pop();
     }
 
-    
-//    auto q = m2d::priority_parallel_queue<std::string>();
-//    q.push("11", 1);
-//    q.push("12", 1);
-//
-//    q.push("21", 2);
-//    q.push("22", 2);
-//
-//    q.push("13", 1);
-//    q.push("14", 1);
-//
-//    q.push("23", 2);
-//    q.push("24", 2);
-//
-//    q.push("31", 3);
-//    q.push("32", 3);
-//
-//    q.push("33", 3);
-//    q.push("25", 2);
-//    q.push("15", 1);
-//
-//    q.push("34", 3);
-//    q.push("35", 3);
-//
-//    while(!q.empty()) {
-//        auto m = q.top();
-//        std::cout << m << std::endl;
-//        q.pop();
-//    }
-
     return 0;
 }
